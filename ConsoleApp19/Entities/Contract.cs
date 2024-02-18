7eb9f880858603807d3a537b19d31aa89da0033c
Victor R. Silva <victorr.silva005@gmail.com>
2024-02-18 18:37:09 -0300

    Created entities classes

---
+++ ConsoleApp19/Entities/Contract.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19.Entities
+{
+    internal class Contract
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+        public List<Installment> Installments { get; set; }
+
+        public Contract() { }
+
+        public Contract(int number, DateTime date, double totalValue, List<Installment> installments)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+            Installments = installments;
+        }
+    }
+}
